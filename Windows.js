6b8bf41b3987193be9accaf0483e9c3a2fb52322
Александра Живушкина < >
2016-05-21 10:45:37 +0300

    Added new files

---
+++ Windows.js
@@ -0,0 +1,12 @@
+/**
+ * Created by ХП on 15.05.2016.
+ */
+function toRegPage() {
+    document.location.href = "Registration.html"
+}
+
+function rollDownRegistration()
+{
+    var reg=document.getElementById("triangle_container");
+    reg.style.visibility="visible"
+}
