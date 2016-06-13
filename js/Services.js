01d6e0ee95b56fc13e9fd1b9ec1559a2474e5dcc
dercius-sdg <dereportinfo@gmail.com>
2016-06-13 12:11:46 +0300

    Change animation for services page

--- js/Services.js
+++ js/Services.js
@@ -7,11 +7,13 @@ $(document).ready(
         var interval=setInterval(
             function()
             {
-                var left_css=$("#photo_gallery").find("ul").css("left");
+                var left_css_string=$("#photo_gallery").find("ul").css("left");
+                left_css_string=left_css_string.substr(0,left_css_string.length-2);
+                var left_css=+left_css_string;
                 if(left_css>-1169) {
                     $("#photo_gallery").find("ul").animate(
                         {
-                            left:left_css-568
+                            left:left_css-586.7
                         },1000
                     );
                 }
@@ -23,7 +25,7 @@ $(document).ready(
                         },1000
                     );
                 }
-            },1000
+            },5000
         )
     }
 );
