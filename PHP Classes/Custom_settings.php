1f8480957930aedc5b45b434f963f127d74fef46
ХП <AleksandaZhivuskina@gmail.com>
2016-06-19 15:44:50 +0300

    Добавлены классы, фото оборудование, изменение диаграммы базы

---
+++ PHP Classes/Custom_settings.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: ХП
+ * Date: 19.06.2016
+ * Time: 15:11
+ */
+require_once($_SERVER['DOCUMENT_ROOT'].'/PHP Classes/DBConnector.php');
+class Custom_settings
+{
+
+}
